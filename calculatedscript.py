93cc56de92bb2bcabcb7451271030dd27f265b92
Animesh-code-6 <tanimesh80@gmail.com>
2025-08-09 00:02:20 +0530

    starting

---
+++ calculatedscript.py
@@ -0,0 +1,11 @@
+
+numbers = [5, 12, 7, 20, 3, 15, 27, 2, 90]
+total_sum = sum(numbers)
+average = total_sum / len(numbers)
+max_value = max(numbers)
+
+
+print("Numbers:", numbers)
+print("Sum:", total_sum)
+print("Average:", average)
+print("Maximum:", max_value)
---
+++ welcomepage.py
@@ -0,0 +1,4 @@
+
+name = "Animesh Tiwari"  
+print("Welcome to Python programming,", name, "!")
+print(f"Hello {name}, glad to have you here!")
